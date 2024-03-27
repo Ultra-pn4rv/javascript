d812e1bb59d7b0b726206df27af7b8145a0e56cc
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 08:42:25 -0400

    questao 11

---
+++ questao11.js
@@ -0,0 +1,5 @@
+/*
+Questão 11: Qual é a diferença entre let, const e var em JavaScript?
+*/
+
+//var e let nao tem diferenca, ja a const tambem é uma variável só que ela precisa de um valor obrigatório
