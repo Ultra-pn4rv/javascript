58767b53f0b25d7a7063ae3d42d3045d9d280c50
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 08:43:32 -0400

    questao 20

---
+++ questao20.js
@@ -0,0 +1,11 @@
+/*
+Questão 20: Como você pode chamar uma função de retorno de chamada em 
+JavaScript?
+*/
+
+function saudacao(nome) {
+    return "Bem vindo, " + nome + "!";
+
+}
+
+console.log(saudacao("Mestre"));
