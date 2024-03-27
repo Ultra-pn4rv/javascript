8eeff604d36157731731406763518894822b5ea9
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 08:47:43 -0400

    funcao de somar

---
+++ funcaoSomar.js
@@ -0,0 +1,21 @@
+//somar
+function somar(num1, num2){
+    return (num1 + num2);
+}
+
+console.log(somar(512, 2873))
+//--------------------------------------------------------
+//multiplicar
+function multiplicacao(num1, num2){
+    return (num1 * num2);
+}
+
+console.log(multiplicacao(512, 2873))
+//--------------------------------------------------------
+//dividir
+
+function dividir(num1, num2){
+    return (num1 / num2);
+}
+
+console.log(dividir(512, 2873))
