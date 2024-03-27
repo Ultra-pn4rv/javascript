16f418932f411d961e391c3d16a88959a0aee5ce
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 08:41:04 -0400

    questao 3

---
+++ questao3.js
@@ -0,0 +1,10 @@
+/*
+Questão 3: Como você pode concatenar duas strings em JavaScript?
+*/
+
+function saudacao(nome) {
+    return "Bem vindo, " + nome + "!";
+
+}
+
+console.log(saudacao("Mestre"));//Saída: Bem vindo, Mestre!
