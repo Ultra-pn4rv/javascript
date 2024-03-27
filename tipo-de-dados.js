35a3adb48bcf66dbc1fb3809fef5c3e48ee07606
Ultra-pn4rv <tsarultra@gmail.com>
2024-03-27 08:49:05 -0400

    Tipo de dados

---
+++ tipo-de-dados.js
@@ -0,0 +1,15 @@
+// var nome = "Felipe";
+// console.log(typeof(nome));
+
+// var caracter = "F";
+// console.log(typeof(caracter));
+// var idade = 25;
+// console.log(typeof(idade));
+
+// var salario = 2500;
+// console.log(typeof(salario));
+
+var a = 10;
+console.log(typeof(a))
+var b = "10";
+console.log(typeof(b))
